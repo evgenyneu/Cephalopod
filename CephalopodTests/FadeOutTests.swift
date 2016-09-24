c8cc263bb3aac38885db2b77679dd8b83d0b4a24
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2016-09-24 16:17:47 +1000

    Rewrite

--- CephalopodTests/FadeOutTests.swift
+++ CephalopodTests/FadeOutTests.swift
@@ -2,29 +2,29 @@ import XCTest
 import AVFoundation
 @testable import Cephalopod
 
-class FadeInTests: XCTestCase {
+class FadeOutTests: XCTestCase {
   
   var player: AVAudioPlayer!
   var cephalopod: Cephalopod!
-    
+  
   override func setUp() {
     super.setUp()
     
     player = TestBundle.soundPlayer()
-    player.volume = 0
+    player.volume = 1
     cephalopod = Cephalopod(player: player)
   }
   
   func testStartVolume() {
-    XCTAssertEqual(0.0, player.volume)
+    XCTAssertEqual(1.0, player.volume)
   }
   
-  func testFadeIn_callFinishClosure() {
+  func testFadeOut_callFinishClosure() {
     let testExpectation = expectation(description: "test expectation")
     
     var finishedArgument: Bool?
     
-    cephalopod.fadeIn(duration: 0.1, velocity: 1, onFinished: { finished in
+    cephalopod.fadeOut(duration: 0.1, velocity: 1, onFinished: { finished in
       finishedArgument = finished
       testExpectation.fulfill()
     })
@@ -32,15 +32,15 @@ class FadeInTests: XCTestCase {
     waitForExpectations(timeout: 0.2) { error in }
     
     XCTAssert(finishedArgument!)
-    XCTAssertEqual(1.0, player.volume)
+    XCTAssertEqual(0.0, player.volume)
   }
   
-  func testFadeIn_cancelByCallingStop_callFinishClosure() {
+  func testFadeOut_cancelByCallingStop_callFinishClosure() {
     let testExpectation = expectation(description: "test expectation")
     
     var finishedArgument: Bool?
     
-    cephalopod.fadeIn(duration: 0.1, velocity: 1, onFinished: { finished in
+    cephalopod.fadeOut(duration: 0.1, velocity: 1, onFinished: { finished in
       finishedArgument = finished
       testExpectation.fulfill()
     })
@@ -50,25 +50,25 @@ class FadeInTests: XCTestCase {
     waitForExpectations(timeout: 0.2) { error in }
     
     XCTAssertFalse(finishedArgument!)
-    XCTAssertEqual(0.0, player.volume)
+    XCTAssertEqual(1.0, player.volume)
   }
   
-  func testFadeIn_cancelByCallingFadeAgain_callFinishClosure() {
+  func testFadeOut_cancelByCallingFadeAgain_callFinishClosure() {
     let testExpectation = expectation(description: "test expectation")
     
     var finishedArgument: Bool?
     
-    cephalopod.fadeIn(duration: 0.1, velocity: 1, onFinished: { finished in
+    cephalopod.fadeOut(duration: 0.1, velocity: 1, onFinished: { finished in
       finishedArgument = finished
       testExpectation.fulfill()
     })
     
-    cephalopod.fadeIn()
+    cephalopod.fadeOut()
     
     waitForExpectations(timeout: 0.2) { error in }
     
     XCTAssertFalse(finishedArgument!)
-    XCTAssertEqual(0.0, player.volume)
+    XCTAssertEqual(1.0, player.volume)
   }
-
+  
 }
