9c9954dc73112c3ecbd370906fea3b511b2da768
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2016-09-24 14:55:18 +1000

    Work

--- Demo-iOS/ViewController.swift
+++ Demo-iOS/ViewController.swift
@@ -9,6 +9,7 @@ class ViewController: UIViewController {
   fileprivate var cephalopod: Cephalopod?
   @IBOutlet weak var sliderParentView: UIView!
   @IBOutlet weak var fadingLabel: UILabel!
+  @IBOutlet weak var squidImage: UIImageView!
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -17,9 +18,11 @@ class ViewController: UIViewController {
     createControls()
     playSound(audioFileName)
     player?.volume = 0
+    squidImage.alpha = 0
 
     if let currentPlayer = player {
       fadeIn(currentPlayer)
+      fadeInSquid()
     }
 
     UILabel.appearance().textColor = UIColor(
@@ -48,6 +51,8 @@ class ViewController: UIViewController {
     if let currentPlayer = player {
       fadeIn(currentPlayer)
     }
+    
+    fadeInSquid()
   }
 
   @IBAction func onFadeOutTapped(_ sender: AnyObject) {
@@ -57,6 +62,8 @@ class ViewController: UIViewController {
     if let currentPlayer = player {
       fadeOut(currentPlayer)
     }
+    
+    fadeOutSquid()
   }
 
   fileprivate func fadeIn(_ aPlayer: AVAudioPlayer) {
@@ -102,6 +109,31 @@ class ViewController: UIViewController {
     }
 
     return Cephalopod(player: player)
+  }
+  
+  func fadeInSquid() {
+    let duration = AppDelegate.current.controls.value(ControlType.duration)
+   
+    UIView.animate(withDuration: duration,
+      delay: 0,
+      options: .beginFromCurrentState,
+      animations: { [weak self] in
+          self?.squidImage.alpha = 1
+        },
+      completion: nil)
+  }
+  
+  func fadeOutSquid() {
+    let duration = AppDelegate.current.controls.value(ControlType.duration)
+    
+    UIView.animate(withDuration: duration,
+     delay: 0,
+     options: .beginFromCurrentState,
+     animations: { [weak self] in
+        self?.squidImage.alpha = 0
+      },
+     completion: nil)
+
   }
 }
 
