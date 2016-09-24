8029007fa3d755692ac9d86455976c5080710914
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2016-09-24 13:23:48 +1000

    Add tvOS

---
+++ Cephalopod-tvOS/Cephalopod-tvOS.h
@@ -0,0 +1,19 @@
+//
+//  Cephalopod-tvOS.h
+//  Cephalopod-tvOS
+//
+//  Created by Evgenii on 24/09/2016.
+//  Copyright © 2016 Evgenii Neumerzhitckii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Cephalopod-tvOS.
+FOUNDATION_EXPORT double Cephalopod_tvOSVersionNumber;
+
+//! Project version string for Cephalopod-tvOS.
+FOUNDATION_EXPORT const unsigned char Cephalopod_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cephalopod_tvOS/PublicHeader.h>
+
+
